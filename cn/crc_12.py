fb1fd8034572eda75a8adf07302fa64be0eb419c
Lonewolf745 <69809921+Lonewolf745@users.noreply.github.com>
2021-02-11 10:35:14 +0530

    Add files via upload

---
+++ cn/crc_12.py
@@ -0,0 +1,42 @@
+def xor(a, b): 
+    result = ''  
+    for i in range(1, len(b)): 
+        if a[i] == b[i]: 
+            result+='0' 
+        else: 
+            result+='1' 
+    return result
+
+def mod2div(divident, divisor): 
+    pick = len(divisor)  
+    tmp = divident[:pick] 
+   
+    while pick < len(divident): 
+   
+        if tmp[0] == '1':  
+            tmp = xor(divisor, tmp) + divident[pick] 
+        else:
+            tmp = xor('0'*pick, tmp) + divident[pick]  
+        pick += 1
+    if tmp[0] == '1': 
+        tmp = xor(divisor, tmp) 
+    else: 
+        tmp = xor('0'*pick, tmp) 
+   
+    checkword = tmp 
+    return checkword 
+
+def encodeData(data, key): 
+   
+    l_key = len(key) 
+    appended_data = data + '0'*(l_key-1) 
+    remainder = mod2div(appended_data, key) 
+    codeword = data + remainder 
+    return codeword 
+
+
+
+polynomial = '1100000001111'
+dataword = input('enter data word:')
+ans = encodeData(dataword,polynomial)
+print(ans)
